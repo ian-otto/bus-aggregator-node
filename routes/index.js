ec09ff0c0635359a06474e9ce9f79016bbfe7672
duper51 <ianotto24@gmail.com>
2018-04-15 15:34:32 -0700

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+
+let get_route_from_stop = (stop_id) => {
+    for(let item of global.links) {
+        if(item.stops_array !== undefined) {
+            for(let obj of item.stops_array) {
+                if(parseInt(stop_id) === obj.stopNumber)
+                    return item;
+            }
+        } else {
+            console.error("stopIdArray undefined");
+        }
+    }
+    return null;
+};
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.json({"version": "1.0"});
+});
+
+router.get('/routes', function (req, res, next) {
+    let retval = [];
+    global.links.forEach(function (current) {
+        retval = retval.concat(current.get_routes());
+    });
+    res.json(retval);
+});
+
+router.get('/stops', function (req, res, next) {
+    let retval = [];
+    global.links.forEach(function (current) {
+        retval = retval.concat(current.get_stops());
+    });
+    res.json(retval);
+});
+
+router.get('/stops/eta/:id', function (req, res, next) {
+    if(!req.params.id || req.params.id < 0) {
+        res.status(400);
+        res.json({"error": "Invalid routeID"});
+        return;
+    }
+    let route = get_route_from_stop(req.params.id);
+    if(!route) {
+        res.status(400);
+        res.json({"error": "Unknown routeID"});
+        return;
+    }
+    route.get_arrival_time(req.params.id, function(err, data) {
+        if(err) {
+            console.log(err);
+            res.json({error: err});
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+router.get('/buses', function (req, res) {
+    let retval = [];
+    global.links.forEach(function (current) {
+        retval = retval.concat(current.bus_array);
+    });
+    res.json(retval);
+});
+
+module.exports = router;
---
+++ test/links/TranslocLinkTest.js
@@ -0,0 +1,75 @@
+let assert = require('assert');
+let TranslocLink = require("../../lib/links/TranslocLink");
+
+describe('TranslocLink', function() {
+    let link = undefined;
+    describe("#construct", function () {
+        it('should not error', function () {
+            link = new TranslocLink();
+            link.fetch_routes();
+        })
+    });
+    describe('#fetch_routes', function() {
+        this.timeout(6000);
+        it('route_array should be larger than one', function(done) {
+            link.on('updated_routes',function () {
+                if(link.route_array.length > 0) done();
+                else done(new Error("Route_array not greater than 1"));
+            });
+        });
+        it('should call #fetch_stops', function (done) {
+            link.on('updated_stops',function () {
+                done();
+            });
+        });
+        it('should call #fetch_shapes', function (done) {
+            link.on('updated_shapes',function () {
+                done();
+            });
+        });
+    });
+    describe('#fetch_stops', function() {
+        this.timeout(4000);
+        it('stops_array should be larger than one', function(done) {
+            if(link.stops_array.length > 0) done();
+            else done(new Error("Route_array not greater than 1"));
+        });
+    });
+    describe('#fetch_shapes', function() {
+        this.timeout(15000);
+        it('shapes_object should be larger than one', function(done) {
+            for(let prop in link.shape_object) {
+                if(link.shape_object.hasOwnProperty(prop)) {
+                    if(link.shape_object[prop].hasOwnProperty('polyline'))
+                        done();
+                    else done(new Error("Length 0 in link props"));
+                    return;
+                }
+            }
+            done(new Error("Route_array not greater than 1"));
+        });
+    });
+    describe("#fetch_buses", function () {
+        this.timeout(4000);
+        it('should call without error', function(done) {
+            try {
+                link.fetch_buses();
+                done();
+            } catch(e) {
+                done(e);
+            }
+        });
+    });
+    describe("#get_arrival_times", function () {
+        it('should not error', function (done) {
+            this.timeout(5000);
+            link.get_arrival_time(24, function(err, data) {
+                if(err) {
+                    done(new Error(err));
+                    return;
+                }
+                done();
+            });
+        });
+    })
+});
